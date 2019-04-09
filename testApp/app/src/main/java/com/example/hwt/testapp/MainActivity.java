aed026ab86dbc451d54f42cfb4937e193ad0bc10
黄文涛 <huangwentao@huangwentaodeMacBook-Pro.local>
2019-04-09 19:08:16 +0800

    init logo and launchimg

--- testApp/app/src/main/java/com/example/hwt/testapp/MainActivity.java
+++ testApp/app/src/main/java/com/example/hwt/testapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
