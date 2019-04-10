42fffb78d13c7b2c69cff10b999e71b01821f0a7
01sr <chenbing8914@outlook.com>
2019-04-10 19:39:19 +0800

    menu

--- app/src/main/java/com/example/hwt/testapp/LaunchActivity.java
+++ app/src/main/java/com/example/hwt/testapp/LaunchActivity.java
@@ -10,7 +10,7 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-//        gotoMainPage();
+        gotoMainPage();
     }
 
     private void gotoMainPage() {
---
+++ app/src/main/java/com/example/hwt/testapp/MainFragment.java
@@ -0,0 +1,24 @@
+package com.example.hwt.testapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cb on 2019/4/10.
+ */
+public class MainFragment extends Fragment {
+
+    public static MainFragment newFragment() {
+        return new MainFragment();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
